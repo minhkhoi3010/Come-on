3fc7485835605b0583e134e87c48b745a0bd4812
minhkhoi3010 <88019399+minhkhoi3010@users.noreply.github.com>
2021-09-16 15:01:39 +0700

    Update config.js (#2)

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hi!',
+    introDesc: `Welcome to thật lòng`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng Khôi đã đẹp trai ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã nhận ra ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Đúng dị <33',
+    btnNo: 'Điên à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Khôi lại đẹp zai thế nhỡ <3',
+    btnReply: 'Gửi cho Khôi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'À, lo mà uống nước đi nhaaaaaaa.',
+    mess: 'Tui biết mà 🥰',
+    messDesc: 'À, nhắn tin cái đi.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.messenger.com/t/100004900952229' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
